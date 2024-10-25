aa51c85deee110b8b8bcc2cf49e4aad32080f919
Berlin Tejeda <berlintejeda22@gmail.com>
2024-10-24 20:50:56 -0400

    Finished part 1

---
+++ index.js
@@ -0,0 +1,38 @@
+//part 1
+const adventurer = {
+  name: "Robin",
+  health: 10,
+  inventory: ["sword", "potion", "artifact"],
+  companion: {
+    name: "Leo",
+    type: "Cat",
+    companion: {
+      name: "Frank",
+      type: "Flea",
+      inventory: ["small hat", "sunglasses"],
+    },
+  },
+  roll(mod = 0) {
+    const result = Math.floor(Math.random() * 20) + 1 + mod;
+    console.log(`${this.name} rolled a ${result}`);
+  },
+};
+
+adventurer.roll();
+
+adventurer.inventory.forEach((item) => {
+  console.log(item);
+});
+
+//part 2
+class Character {
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+    this.inventory = [];
+  }
+  roll = function (mod = 0) {
+    const result = Math.floor(Math.random() * 20) + 1 + mod;
+    console.log(`${this.name} rolled a ${result}`);
+  };
+}
