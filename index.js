069f811bd0abb5d6535075ee50607122a6a21426
Berlin Tejeda <berlintejeda22@gmail.com>
2024-10-24 21:02:43 -0400

    Finished part 3

--- index.js
+++ index.js
@@ -36,3 +36,38 @@ class Character {
     console.log(`${this.name} rolled a ${result}`);
   };
 }
+
+const robin = new adventurer("Robin");
+robin.inventory = ["sword", "potion", "artifact"];
+robin.companion = new Companion("Leo");
+robin.companion.type = "Cat";
+robin.companion.companion = new Companion("Frank");
+robin.companion.companion.type = "Flea";
+robin.companion.companion.inventory = ["small hat", "sunglasses"];
+
+class Adventurer extends Character {
+  constructor(name, role) {
+    super(name);
+    // Adventurers have specialized roles.
+    this.role = role;
+    // Every adventurer starts with a bed and 50 gold coins.
+    this.inventory.push("bedroll", "50 gold coins");
+  }
+  // Adventurers have the ability to scout ahead of them.
+  scout() {
+    console.log(`${this.name} is scouting ahead...`);
+    super.roll();
+  }
+}
+
+class Companion extends Character {
+  constructor(name, role) {
+    super(name);
+    this.role = role;
+    this.inventory.push("bedroll", "50 gold coins");
+  }
+  scout() {
+    console.log(`${this.name} is scouting ahead...`);
+    super.roll();
+  }
+}
